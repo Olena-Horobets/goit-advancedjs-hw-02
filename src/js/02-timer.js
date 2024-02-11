2b7e220bbaf3a3436b628fff552e522309eed962
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-02-11 18:34:43 -0330

    timer added

---
+++ src/js/02-timer.js
@@ -0,0 +1,59 @@
+import flatpickr from 'flatpickr';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    let difference = selectedDates[0] - new Date();
+    if (difference <= 0) {
+      return window.alert('Please choose a date in the future');
+    }
+
+    time = selectedDates[0];
+  },
+};
+
+flatpickr(refs.input, options);
+let time = 0;
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+refs.startBtn.addEventListener('click', e =>
+  setInterval(() => updateTimer(convertMs(time - new Date())), 1000)
+);
